357257de4ae5c5ad9fb774492708711d712da062
nishan-singh <singh007nishan@gmail.com>
2025-06-22 21:46:40 +0200

    create table for files

--- frontend/src/components/Files.vue
+++ frontend/src/components/Files.vue
@@ -83,11 +83,25 @@ getFiles();
 </script>
 
 <style scoped>
-.file-list li {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 10px;
+table.files {
+    width: 100%;
+    border-collapse: collapse;
+    margin-top: 20px;
+}
+
+table.files th,
+table.files td {
+    padding: 12px;
+    text-align: left;
+    border: 1px solid #ddd;
+}
+
+table.files tr:nth-child(even) {
+    background-color: #f2f2f2;
+}
+
+table.files tr:hover {
+    background-color: #ddd;
 }
 
 .download-button {
@@ -167,32 +181,42 @@ input[type="file"]::file-selector-button:active {
                             stroke-linecap="round" stroke-linejoin="round"></path>
                     </g>
                 </svg>
-                <input type="file" id="myFile" name="filename" ref="fileInput" class="inputfile" 
-                    @dragenter="highlight = true"
-                    @drop="highlight = false"
-                />
+                <input type="file" id="myFile" name="filename" ref="fileInput" class="inputfile"
+                    @dragenter="highlight = true" @drop="highlight = false" />
             </p>
             <button type="submit" class="default upload-btn">Upload</button>
         </form>
         <hr />
         <button @click="getFiles">Refresh Files</button>
-        <ul class="file-list">
-            <!-- Assuming files is an array of file objects -->
-            <li v-for="file in files" :key="file.id">
-                {{ file.originalname }} - {{ file.size }} bytes
-                <button class="download-button" @click="downloadFile(file.name)">
-                    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-                        <g id="SVGRepo_iconCarrier">
-                            <path
-                                d="M17 17H17.01M17.4 14H18C18.9319 14 19.3978 14 19.7654 14.1522C20.2554 14.3552 20.6448 14.7446 20.8478 15.2346C21 15.6022 21 16.0681 21 17C21 17.9319 21 18.3978 20.8478 18.7654C20.6448 19.2554 20.2554 19.6448 19.7654 19.8478C19.3978 20 18.9319 20 18 20H6C5.06812 20 4.60218 20 4.23463 19.8478C3.74458 19.6448 3.35523 19.2554 3.15224 18.7654C3 18.3978 3 17.9319 3 17C3 16.0681 3 15.6022 3.15224 15.2346C3.35523 14.7446 3.74458 14.3552 4.23463 14.1522C4.60218 14 5.06812 14 6 14H6.6M12 15V4M12 15L9 12M12 15L15 12"
-                                stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-                        </g>
-                    </svg>
-                </button>
-            </li>
-        </ul>
+        <table class="files">
+            <thead>
+                <tr>
+                    <th>File Name</th>
+                    <th>Size (bytes)</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="file in files" :key="file.id">
+                    <td>{{ file.originalname }}</td>
+                    <td>{{ file.size }}</td>
+                    <td>
+                        <button class="download-button" @click="downloadFile(file.name)">
+                            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                                <g id="SVGRepo_iconCarrier">
+                                    <path
+                                        d="M17 17H17.01M17.4 14H18C18.9319 14 19.3978 14 19.7654 14.1522C20.2554 14.3552 20.6448 14.7446 20.8478 15.2346C21 15.6022 21 16.0681 21 17C21 17.9319 21 18.3978 20.8478 18.7654C20.6448 19.2554 20.2554 19.6448 19.7654 19.8478C19.3978 20 18.9319 20 18 20H6C5.06812 20 4.60218 20 4.23463 19.8478C3.74458 19.6448 3.35523 19.2554 3.15224 18.7654C3 18.3978 3 17.9319 3 17C3 16.0681 3 15.6022 3.15224 15.2346C3.35523 14.7446 3.74458 14.3552 4.23463 14.1522C4.60218 14 5.06812 14 6 14H6.6M12 15V4M12 15L9 12M12 15L15 12"
+                                        stroke="#000000" stroke-width="2" stroke-linecap="round"
+                                        stroke-linejoin="round"></path>
+                                </g>
+                            </svg>
+                        </button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
         <p v-if="files.length === 0">No files available.</p>
     </section>
 
