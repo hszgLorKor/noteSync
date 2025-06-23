b9290b4591b1c446f287dbcb33db9565d046c00b
nishan-singh <singh007nishan@gmail.com>
2025-06-23 10:50:29 +0200

    add delete button for each file

--- frontend/src/components/Files.vue
+++ frontend/src/components/Files.vue
@@ -53,6 +53,21 @@ function uploadFile() {
         })
 }
 
+function deleteFile(fileName) {
+    fetch(`https://lobes.it/api/delete/${fileName}`, {
+        method: 'DELETE',
+        credentials: 'include'
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('File deleted:', data);
+            getFiles(); // Refresh the file list after deletion
+        })
+        .catch((error) => {
+            console.error('Error deleting file:', error);
+        });
+}
+
 // Function to fetch files for the current lecture
 const getFiles = async () => {
     try {
@@ -83,10 +98,15 @@ getFiles();
 </script>
 
 <style scoped>
+.files-list {
+    max-width: 98vw;
+}
+
 table.files {
     width: 100%;
     border-collapse: collapse;
     margin-top: 20px;
+    overflow-x: auto;
 }
 
 table.files th,
@@ -110,7 +130,7 @@ table.files tr:hover {
     cursor: pointer;
 }
 
-.download-button svg {
+.download-button svg, .delete-button svg {
     width: 32px;
     height: 32px;
 }
@@ -188,35 +208,54 @@ input[type="file"]::file-selector-button:active {
         </form>
         <hr />
         <button @click="getFiles">Refresh Files</button>
-        <table class="files">
-            <thead>
-                <tr>
-                    <th>File Name</th>
-                    <th>Size (bytes)</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="file in files" :key="file.id">
-                    <td>{{ file.originalname }}</td>
-                    <td>{{ file.size }}</td>
-                    <td>
-                        <button class="download-button" @click="downloadFile(file.name)">
-                            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-                                <g id="SVGRepo_iconCarrier">
-                                    <path
-                                        d="M17 17H17.01M17.4 14H18C18.9319 14 19.3978 14 19.7654 14.1522C20.2554 14.3552 20.6448 14.7446 20.8478 15.2346C21 15.6022 21 16.0681 21 17C21 17.9319 21 18.3978 20.8478 18.7654C20.6448 19.2554 20.2554 19.6448 19.7654 19.8478C19.3978 20 18.9319 20 18 20H6C5.06812 20 4.60218 20 4.23463 19.8478C3.74458 19.6448 3.35523 19.2554 3.15224 18.7654C3 18.3978 3 17.9319 3 17C3 16.0681 3 15.6022 3.15224 15.2346C3.35523 14.7446 3.74458 14.3552 4.23463 14.1522C4.60218 14 5.06812 14 6 14H6.6M12 15V4M12 15L9 12M12 15L15 12"
-                                        stroke="#000000" stroke-width="2" stroke-linecap="round"
-                                        stroke-linejoin="round"></path>
-                                </g>
-                            </svg>
-                        </button>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+        <div class="file-list">
+            <table class="files">
+                <thead>
+                    <tr>
+                        <th>File Name</th>
+                        <th>Size (bytes)</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="file in files" :key="file.id">
+                        <td>{{ file.originalname }}</td>
+                        <td>{{ file.size }}</td>
+                        <td>
+                            <button class="download-button" @click="downloadFile(file.name)">
+                                <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                                    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                                    <g id="SVGRepo_iconCarrier">
+                                        <path
+                                            d="M17 17H17.01M17.4 14H18C18.9319 14 19.3978 14 19.7654 14.1522C20.2554 14.3552 20.6448 14.7446 20.8478 15.2346C21 15.6022 21 16.0681 21 17C21 17.9319 21 18.3978 20.8478 18.7654C20.6448 19.2554 20.2554 19.6448 19.7654 19.8478C19.3978 20 18.9319 20 18 20H6C5.06812 20 4.60218 20 4.23463 19.8478C3.74458 19.6448 3.35523 19.2554 3.15224 18.7654C3 18.3978 3 17.9319 3 17C3 16.0681 3 15.6022 3.15224 15.2346C3.35523 14.7446 3.74458 14.3552 4.23463 14.1522C4.60218 14 5.06812 14 6 14H6.6M12 15V4M12 15L9 12M12 15L15 12"
+                                            stroke="#000000" stroke-width="2" stroke-linecap="round"
+                                            stroke-linejoin="round"></path>
+                                    </g>
+                                </svg>
+                            </button>
+                            <button class="delete-button" @click="deleteFile(file.name)">
+                                <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                                    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                                    <g id="SVGRepo_iconCarrier">
+                                        <path d="M4 7H20" stroke="#000000" stroke-width="2" stroke-linecap="round"
+                                            stroke-linejoin="round"></path>
+                                        <path
+                                            d="M6 10L7.70141 19.3578C7.87432 20.3088 8.70258 21 9.66915 21H14.3308C15.2974 21 16.1257 20.3087 16.2986 19.3578L18 10"
+                                            stroke="#000000" stroke-width="2" stroke-linecap="round"
+                                            stroke-linejoin="round"></path>
+                                        <path d="M9 5C9 3.89543 9.89543 3 11 3H13C14.1046 3 15 3.89543 15 5V7H9V5Z"
+                                            stroke="#000000" stroke-width="2" stroke-linecap="round"
+                                            stroke-linejoin="round"></path>
+                                    </g>
+                                </svg>
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
         <p v-if="files.length === 0">No files available.</p>
     </section>
 
